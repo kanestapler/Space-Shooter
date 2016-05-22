8dac3e81b1bce7e627ab6b57fdbd8b0f5680c03f
kanestapler <kanestapler@gmail.com>
2016-05-22 14:51:22 -0500

    hazards spawn in waves now

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,16 +5,26 @@ public class GameController : MonoBehaviour {
 
 	public GameObject hazard;
 	public Vector3 spawnPosition;
+	public int spawnsInWave;
+	public float spawnWait;
+	public float startWait;
 
 	// Use this for initialization
 	void Start () {
-		SpawnWaves ();
+		StartCoroutine (SpawnWaves());
 	}
 
-	void SpawnWaves() {
-		Instantiate (
-			hazard,
-			new Vector3(Random.Range(-spawnPosition.x, spawnPosition.x), spawnPosition.y, spawnPosition.z),
-			Quaternion.identity);
+	IEnumerator SpawnWaves() {
+
+		while (true) {
+			yield return new WaitForSeconds (startWait);
+			for (int i = 0; i < spawnsInWave; i++) {
+				Instantiate (
+					hazard,
+					new Vector3 (Random.Range (-spawnPosition.x, spawnPosition.x), spawnPosition.y, spawnPosition.z),
+					Quaternion.identity);
+				yield return new WaitForSeconds (spawnWait);
+			}
+		}
 	}
 }
